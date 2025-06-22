c5de9ef7869a60b607ebea1f4b1e6b2af0fe9dc9
kangrisboom <kangrisboom@gmail.com>
2025-06-23 05:58:23 +0700

    fix1

---
+++ target/linux/rk322x/image/armv7.mk
@@ -0,0 +1,7 @@
+define Device/Default
+  PROFILES := Default
+  SOC := rk3229
+  DEVICE_VENDOR := Generic
+  UBOOT_DEVICE_NAME := rk3229
+  DEVICE_PACKAGES := kmod-usb-core kmod-usb2 kmod-usb-ohci kmod-usb-ehci
+endef
